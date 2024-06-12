175cd4821dc0d50164ac970f8f05ef3c1eae36d6
willhsu <wlhsu1992@hotmail.com>
2024-06-12 23:39:23 +0800

    feat: 調整首頁顯示介面 & 移除導覽選單

--- AccountingApp/Program.cs
+++ AccountingApp/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Expenditures}/{action=Index}/{id?}");
 
 app.Run();
